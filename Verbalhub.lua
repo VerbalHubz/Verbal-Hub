1422cf165cbff062577aab9c1fb62d6ae9fe82bb
VerbalHubz <nuviadiaz1005@gmail.com>
2024-09-28 20:48:39 -0400

    Update Verbalhub.lua

--- Verbalhub.lua
+++ Verbalhub.lua
@@ -1,3 +1,17 @@
+-- Define the message to display when the player is kicked
+local message = "Horomori Anti-Cheat has kicked you. Please stop hacking."
 
+-- Get the local player (the player executing the script)
+local player = game.Players.LocalPlayer
 
-game.Players.LocalPlayer:Kick(“LOL ITS DOWN STUPID NIGGER”)
+-- Function to display the message and kick the player
+local function kickPlayer()
+    -- Display the message
+    print(message)
+    
+    -- Kick the player with the message
+    player:Kick(message)
+end
+
+-- Execute the function to kick the player
+kickPlayer()
